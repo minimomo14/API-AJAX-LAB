ce2eac54492e46ee8800c2f0005f1a8182d6036e
Momo Miller <minimomo14z@hotmail.com>
2021-12-09 20:51:53 -0500

    1st commit API/AJAX still working on css

---
+++ newScript.js
@@ -0,0 +1,35 @@
+const body = document.querySelector("body");
+
+const awwPromise = fetch("https://www.reddit.com/r/aww/.json")
+.then(res => res.json())
+
+awwPromise.then(data => {
+    for (let i = 0; i < data.data.children.length; i++){
+        const newDiv = document.createElement("div")
+        const newTitle = document.createElement("h3")
+        const newImage = document.createElement("img")
+        const newLink = document.createElement("a")
+
+        const Title = document.querySelector("h3")
+        const Image = document.querySelector("img")
+        const Link = document.querySelector("a")
+
+        // let name = data.data.children[i].data.name
+        let title = data.data.children[i].data.title
+        let image = data.data.children[i].data.thumbnail
+        let link = data.data.children[i].data.url
+
+       newTitle.innerHTML = title;
+       newImage.src = image;
+       newLink.href = link;
+       newLink.innerText = link;
+
+    //    Link.setAttribute("href", ${link});
+
+    body.append(newDiv); 
+    newDiv.appendChild(newTitle);
+    newDiv.appendChild(newImage);
+    newDiv.appendChild(newLink);
+ }
+ 
+});
---
+++ script.js
@@ -0,0 +1,33 @@
+/// First Version 
+console.log("working !!!")
+
+// 1.fetch 2..then 3.json 4 console.log to check data
+const body = document.querySelector("#body");
+
+//const awwPromise = 
+fetch ("https://www.reddit.com/r/aww/.json")
+.then(res => res.json())
+
+//awwPromise
+.then(data => {
+    //console.log(data.data.children[0]);
+    for (let i=0; i < data.data.children.length; i++){
+        const newDiv = document.createElement("div");
+        //name title link image
+        let name = data.data.children[i].data.name
+        let title = data.data.children[i].data.title
+        let link = data.data.children[i].data.url
+        let image = data.data.children[i].data.thumbnail
+
+        //console.log(name);
+
+        // <br> use it to break the line 
+        newDiv.innerHTML = `<br> ${name} <br> ${title} <br> ${link} <br> ${image} <br>`;
+        // or other way you can write => newDiv.innerHTML = name + " " + title + " " + link + " " + image;
+        body.appendChild(newDiv); 
+
+        //console.log(data.data.children[i]);
+    };
+
+    
+});
